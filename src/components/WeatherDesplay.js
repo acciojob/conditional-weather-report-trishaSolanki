aef3d55825b386983c3235d803c3152fc9f61d37
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-07-10 10:11:44 +0000

    g

--- src/components/WeatherDesplay.js
+++ src/components/WeatherDesplay.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function WeatherDisplay({ weather }) {
   const { temperature, conditions } = weather;
-  const temperatureColor = temperature > 20 ? 'rgb(255,0,0)' : 'blue';
+  const temperatureColor = temperature > 20 ? 'red' : 'blue';
 
   return (
     <div>
-     <span style={{ color: temperatureColor }}>Temperature: {temperature}</span> <br></br>
+     <p style={{ color: temperatureColor }}>Temperature: {temperature}</p>
      <span>Conditions: {conditions}</span>
     </div>
   );
