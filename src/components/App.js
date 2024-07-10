9e45f9dc93e41cfc48ee659de98fb33bd86a879d
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-07-10 09:51:55 +0000

    d

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,24 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+// src/components/App.js
+import React, { useState, useEffect } from 'react';
+import WeatherDisplay from '././WeatherDesplay';
+
+function App() {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: '' });
+
+  useEffect(() => {
+    const weatherInput = { temperature: 25, conditions: "Sunny" };
+    setWeather(weatherInput);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
---
+++ src/components/WeatherDesplay.js
@@ -0,0 +1,16 @@
+// src/components/WeatherDisplay.js
+import React from 'react';
+
+function WeatherDisplay({ weather }) {
+  const { temperature, conditions } = weather;
+  const temperatureColor = temperature > 20 ? 'red' : 'blue';
+
+  return (
+    <div>
+      <p style={{ color: temperatureColor }}>Temperature: {temperature}</p>
+      <span>Conditions: {conditions}</span>
+    </div>
+  );
+}
+
+export default WeatherDisplay;
